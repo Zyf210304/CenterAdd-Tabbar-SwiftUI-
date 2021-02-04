2c73c808fb1ef6ae2d44729f3f380e9eecb9685a
张亚飞 <672233580@qq.com>
2021-02-04 20:48:09 +0800

    Initial Commit

---
+++ CenterAdd Tabbar/CenterAdd_TabbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CenterAdd_TabbarApp.swift
+//  CenterAdd Tabbar
+//
+//  Created by 张亚飞 on 2021/2/4.
+//
+
+import SwiftUI
+
+@main
+struct CenterAdd_TabbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CenterAdd Tabbar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CenterAdd Tabbar
+//
+//  Created by 张亚飞 on 2021/2/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
